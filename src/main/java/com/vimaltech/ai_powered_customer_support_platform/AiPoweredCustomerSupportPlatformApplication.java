85776c00ce164916123f7864956b645ed6f6f9ee
vimal-java-dev <vimal929@gmail.com>
2026-06-14 16:26:12 +0530

    Initial Spring Boot project commit
    Signed-off-by: vimal-java-dev <vimal929@gmail.com>

---
+++ src/main/java/com/vimaltech/ai_powered_customer_support_platform/AiPoweredCustomerSupportPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.vimaltech.ai_powered_customer_support_platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiPoweredCustomerSupportPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AiPoweredCustomerSupportPlatformApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vimaltech/ai_powered_customer_support_platform/AiPoweredCustomerSupportPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vimaltech.ai_powered_customer_support_platform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiPoweredCustomerSupportPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
