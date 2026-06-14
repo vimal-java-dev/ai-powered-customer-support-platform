ef6a747fd645bec898c82bfc7940d5de3c9daba4
vimal-java-dev <vimal929@gmail.com>
2026-06-15 01:42:56 +0530

    setup spring boot project and postgres connection
    Signed-off-by: vimal-java-dev <vimal929@gmail.com>

--- src/main/java/com/vimaltech/ai_powered_customer_support_platform/AiPoweredCustomerSupportPlatformApplication.java
+++ src/main/java/com/vimaltech/ai_powered_customer_support_platform/AiPoweredCustomerSupportPlatformApplication.java
@@ -3,10 +3,18 @@ package com.vimaltech.ai_powered_customer_support_platform;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.time.ZoneId;
+import java.util.TimeZone;
+
 @SpringBootApplication
 public class AiPoweredCustomerSupportPlatformApplication {
 
 	public static void main(String[] args) {
+
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Kolkata"));
+
+		System.out.println(TimeZone.getDefault().getID());
+
 		SpringApplication.run(AiPoweredCustomerSupportPlatformApplication.class, args);
 	}
 
